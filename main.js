8664a81720fccd364db459998998c5c342eaaec9
Luis <luiscastro193@gmail.com>
2022-07-02 14:23:52 +0200

    Clean code

--- main.js
+++ main.js
@@ -1,22 +1,44 @@
 "use strict";
-function getHandRects(handResults, settings) {
-	return handResults.multiHandLandmarks.map(landmarks => {
-		let x = landmarks.map(mark => mark.x);
-		let y = landmarks.map(mark => mark.y);
-		let x_max = Math.max(...x) * settings.width;
-		let x_min = Math.min(...x) * settings.width;
-		let y_max = Math.max(...y) * settings.height;
-		let y_min = Math.min(...y) * settings.height;
-		
-		return [x_min, y_min, x_max - x_min, y_max - y_min];
-	})
-}
+const drawElements = []
 
-function drawHands(handResults, ctx, settings) {
-	ctx.clearRect(0, 0, settings.width, settings.height);
+class HandRects {
+	constructor(video, ctx, settings) {
+		const self = this;
+		this.ctx = ctx;
+		this.settings = settings;
+		this.rects = [];
+		
+		this.hands = new Hands({locateFile: file => `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`});
+		this.hands.setOptions({modelComplexity: 1});
+		this.hands.onResults(results => this.update(results));
+		
+		async function sendImage() {
+			await self.hands.send({image: video});
+			requestAnimationFrame(sendImage);	
+		}
+		
+		requestAnimationFrame(sendImage);
+	}
 	
-	for (let rect of getHandRects(handResults, settings))
-		ctx.strokeRect(...rect);
+	update(results) {
+		this.rects = results.multiHandLandmarks.map(landmarks => {
+			let x = landmarks.map(mark => mark.x);
+			let y = landmarks.map(mark => mark.y);
+			let x_max = Math.max(...x) * this.settings.width;
+			let x_min = Math.min(...x) * this.settings.width;
+			let y_max = Math.max(...y) * this.settings.height;
+			let y_min = Math.min(...y) * this.settings.height;
+			
+			return [x_min, y_min, x_max - x_min, y_max - y_min];
+		});
+	}
+	
+	draw() {
+		for (let rect of this.rects)
+			this.ctx.strokeRect(...rect);
+		
+		return true;
+	}
 }
 
 navigator.mediaDevices.getUserMedia({audio: false, video: {width: 1280, height: 720}}).then(stream => {
@@ -29,12 +51,11 @@ navigator.mediaDevices.getUserMedia({audio: false, video: {width: 1280, height: 
 	canvas.height = settings.height;
 	video.srcObject = stream;
 	
-	const hands = new Hands({locateFile: file => `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`});
-	hands.setOptions({modelComplexity: 1});
-	hands.onResults(results => drawHands(results, ctx, settings));
+	drawElements.push(new HandRects(video, ctx, settings));
 	
 	async function draw() {
-		await hands.send({image: video});
+		ctx.clearRect(0, 0, settings.width, settings.height);
+		drawElements.filter(element => element.draw());
 		requestAnimationFrame(draw);	
 	}
 	
