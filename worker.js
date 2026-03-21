79b200dcbbce250150c317c3a6c3846f052d2fbe
luiscastro193 <luiscastro193@gmail.com>
2026-03-21 10:52:13 +0100

    Ensure same version

--- worker.js
+++ worker.js
@@ -1,18 +1,20 @@
 "use strict";
-import("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision/vision_bundle.mjs").then(({FilesetResolver, HandLandmarker}) =>
-	FilesetResolver.forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision/wasm").then(vision =>
-		HandLandmarker.createFromOptions(vision, {
-			baseOptions: {
-				modelAssetPath: "https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/latest/hand_landmarker.task",
-				delegate: "GPU"
-			},
-			runningMode: "VIDEO",
-			numHands: 4,
-			minHandDetectionConfidence: 0.3,
-			minTrackingConfidence: 0.2
-		})
-	)
-).then(hands => {
+const base = "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision";
+const moduleUrl = `${base}/vision_bundle.mjs`;
+import(moduleUrl).then(async ({FilesetResolver, HandLandmarker}) => {
+	const version = (await fetch(moduleUrl, {cache: "force-cache"})).headers.get("x-jsd-version");
+	const vision = await FilesetResolver.forVisionTasks(`${base}@${version}/wasm`);
+	return HandLandmarker.createFromOptions(vision, {
+		baseOptions: {
+			modelAssetPath: "https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/latest/hand_landmarker.task",
+			delegate: "GPU"
+		},
+		runningMode: "VIDEO",
+		numHands: 4,
+		minHandDetectionConfidence: 0.3,
+		minTrackingConfidence: 0.2
+	});
+}).then(hands => {
 	postMessage(null);
 	
 	onmessage = ({data: frame}) => {
